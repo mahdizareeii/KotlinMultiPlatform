93b43105db07f79cd6c3b46feccee41d7e2ecc2b
mahdizareei <m.zareei@jabama.com>
2024-02-26 14:16:40 +0330

    load data in ios

--- iosApp/iosApp/component/ProductComponent.swift
+++ iosApp/iosApp/component/ProductComponent.swift
@@ -13,15 +13,21 @@ import SwiftUI
 struct ProductComponent: View {
     var data : ProductComponentData
     var body : some View {
-        HStack {
-            AsyncImage(url: URL(string: data.image)).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 100).cornerRadius(5)
-            VStack{
+        HStack(alignment:.top){
+            AsyncImage(url: URL(string: data.image))
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 100)
+                .cornerRadius(5)
+            
+            Spacer().frame(width: 0).padding(3)
+            
+            VStack(alignment: .leading){
                 Text(data.title)
                     .font(Font.system(size: 14,weight: .bold))
                     .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
                     .truncationMode(/*@START_MENU_TOKEN@*/.tail/*@END_MENU_TOKEN@*/)
                 
-                Spacer().frame(height: 3)
+                Spacer().frame(height: 0).padding(3)
+                
                 Text(data.description)
                     .font(Font.system(size: 10, weight: .light))
                     .lineLimit(3)
